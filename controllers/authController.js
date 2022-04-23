c735797dbedb0d31672b6fdd8e91202007521902
sangenispablo <sangenispablo@gmail.com>
2022-04-23 11:06:47 -0300

    fix valid error

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,19 @@
 import { request, response } from "express";
 import { StatusCodes } from "http-status-codes";
 
+import { BadRequestError } from '../errors/index.js'
+
 import User from "../models/User.js";
 
 const register = async (req = request, res = response) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    throw new BadRequestError('Please provide all values');
+  }
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError('El email ya existe con otro usuario');
+  }
   const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ user });
 };
